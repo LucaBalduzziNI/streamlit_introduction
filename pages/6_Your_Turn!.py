379efd5612f59428bb319feba72f3eeadfbb2e1b
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2024-02-14 14:53:28 +0100

    Spell checks

--- pages/6_Your_Turn!.py
+++ pages/6_Your_Turn!.py
@@ -9,11 +9,11 @@ st.set_page_config(
 st.title('Your Turn!')
 st.divider()
 
-st.write('Try to implement the followong behaviour:')
+st.subheader('Try to implement the followong behaviour:')
 st.write('- Three tabs, one containing a file uploader, one the uploaded dataframe and one containing settings')
 st.write('- In the uploaded dataframe tab, show the uploaded file as an editable dataframe')
 st.write('- In the settings tab implement a selectbox to select the method (json, csv, etc.) to save the modified file')
 st.write('- At the bottom of uploaded dataframe tab, create a button to save the file in the format choosen in the settings tab')
-st.write('Bonus Points:')
+st.subheader('Bonus Points:')
 st.write('- Remake the app to be on multiple pages')
 st.write('- Implement a "Discard Changes" button')
